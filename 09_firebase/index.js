3233fd7d7b253987f5da8f0ff9c86d0f9a6ba23b
haloman151 <sebastianSDrejer@gmail.com>
2026-03-26 09:40:45 +0100

    firebase project

--- 09_firebase/index.js
+++ 09_firebase/index.js
@@ -1,6 +1,18 @@
 
 
+//lev en ref til din collection
+var testRef = db.collection('test')
+console.log('oprettet ref til test')
+
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
+    //nu kommer det geniale: onSnapShot
+    testRef.onSnapshot( snap => {
+        console.log('modtog snap', snap.size)
+        snap.forEach( doc => {
+            var d = doc.data()
+            console.log(d)
+        })
+    })
 }
 
